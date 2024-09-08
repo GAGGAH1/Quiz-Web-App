726172f9cf1519c18aa3f4a03dc9e06591f94995
Titus <titusdcstd2022@gmail.com>
2024-09-08 21:39:11 +0100

    Added validation for the input field

--- index.js
+++ index.js
@@ -2,9 +2,21 @@
 const registrationForm = document.getElementById("signup-form");
 const fineMessage = document.getElementById("fine-message");
 const deadline = new Date("2024-10-01");
+const errorMessage = document.getElementById('error-message')
 
 registrationForm.addEventListener("submit", function(event) {
     event.preventDefault();
+
+    const school = document.getElementById("school-name").value.trim();
+    const candidate = document.getElementById("candidate-name").value.trim();
+    // Validate input fields
+    if (!school || !candidate) {
+        errorMessage.textContent = "Both School Name and Candidate Name are required!";
+        errorMessage.classList.remove("hidden");
+        return;
+    } else {
+        errorMessage.classList.add("hidden"); 
+    }
     const now = new Date();
     if (now > deadline) {
         fineMessage.classList.remove("hidden");
@@ -12,9 +24,6 @@ registrationForm.addEventListener("submit", function(event) {
         fineMessage.classList.add("hidden");
         alert("Registration Successful");
         // Save registration info to localStorage
-        const school = document.getElementById("school-name").value;
-        const candidate = document.getElementById("candidate-name").value;
-        
         localStorage.setItem("registered", JSON.stringify({ school, candidate }));
         // alert("Successfully Registered")
         window.location.href = 'quiz.html' 
